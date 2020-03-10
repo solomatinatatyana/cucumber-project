e2780d6050711cd8c8fe935515f25f1fb099dbb1
tsolomatina <Hfgbl123>
2020-03-10 12:21:09 +0300

    Initial commit

---
+++ src/main/java/ru/otus/cucumberproject/CucumberProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.cucumberproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/cucumberproject/CucumberProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.cucumberproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
